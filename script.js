9cda5faa6b915c97bd759d4f677defa1db6c703c
Avishka Nilupul <avishkamine@gmail.com>
2023-05-09 23:19:37 +0530

    test2

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+
 //Unplash API
 
 const count = 10;
@@ -5,13 +10,20 @@ const apiKey = "PtmLBD7g8lFtf48jiXscnHK3QcWj4TTZ85zqtXQntJc";
 const apiURL = `https://api.unsplash.com/photos/random/
 ?client_id=${apiKey}&count=${count}`;
 
-//Get photos from Unsplah API
 
+// Creare Elements For Links and Photos
+function displayPhotos(){
+  photosArray.forEach((photo) =>{
+    
+  })
+}
+
+//Get photos from Unsplah API
 async function getPhotos() {
   try {
     const response = await fetch(apiURL);
-    const data = await response.json();
-    console.log(data);
+  photosArray = await response.json();
+  displayPhotos();
   } catch (error) {
     //catch error
   }
